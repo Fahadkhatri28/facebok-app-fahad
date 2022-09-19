98d60c5c2d6479592a6d6a9909b5617e6a018f7c
Syedmoiz7 <syedzada7866@gmail.com>
2022-09-19 22:39:28 +0500

    facebok-app

---
+++ src/components/rightBar.jsx
@@ -0,0 +1,66 @@
+// import { FaBell, FaSearch, FaFacebook, FaHome, FaUserFriends, FaStore, FaChessBoard, FaFacebookMessenger } from "react-icons/fa";
+// import { MdOutlinePersonalVideo } from "react-icons/md";
+// import { TbGridDots } from "react-icons/tb";
+const rightBar = () => {
+    return (
+        <div className="bsb-3">
+
+            <div className="contact">
+                <p>Contacts
+                    <i className="fa-solid fa-video v-icon"></i>
+                    <i className="fa-solid fa-magnifying-glass "></i>
+                    <i className="fa-solid fa-ellipsis"></i>
+                </p>
+            </div>
+
+            <div type="" className="bowling-icon">
+                <img src="./imgs/cp-1_28x28.jpg" alt="" className="" />
+                <p>Baber Azam</p>
+            </div>
+
+            <div type="" className="bowling-icon">
+                <img src="./imgs/cp-2_28x28.jpg" alt="" className="" />
+                <p>Asif Ali</p>
+            </div>
+
+            <div type="" className="bowling-icon">
+                <img src="./imgs/cp-3_28x28.jpg" alt="" className="" />
+                <p>Shadab khan</p>
+            </div>
+
+            <div type="" className="bowling-icon">
+                <img src="./imgs/cp-1_28x28.jpg" alt="" className="" />
+                <p>Shaheen</p>
+            </div>
+
+            <div type="" className="bowling-icon">
+                <img src="./imgs/cp-2_28x28.jpg" alt="" className="" />
+                <p>Khusdil</p>
+            </div>
+
+            <div type="" className="bowling-icon">
+                <img src="./imgs/cp-3_28x28.jpg" alt="" className="" />
+                <p>Iftqar</p>
+            </div>
+
+            <div type="" className="bowling-icon">
+                <img src="./imgs/cp-1_28x28.jpg" alt="" className="" />
+                <p>rizwan</p>
+            </div>
+
+            <div type="" className="bowling-icon">
+                <img src="./imgs/cp-2_28x28.jpg" alt="" className="" />
+                <p>haris rauf</p>
+            </div>
+
+            <div type="" className="bowling-icon">
+                <img src="./imgs/cp-3_28x28.jpg" alt="" className="" />
+                <p>haider ali</p>
+            </div>
+
+        </div>
+
+    );
+}
+
+export default rightBar;
